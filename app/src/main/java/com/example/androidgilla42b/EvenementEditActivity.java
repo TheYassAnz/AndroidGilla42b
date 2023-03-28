0a61e7407e91a776a2f0418c67056f967cdea928
yassanzWebmaster <yassanz.webmaster@gmail.com>
2023-03-28 11:57:38 +0200

    TP3 finished

--- app/src/main/java/com/example/androidgilla42b/EvenementEditActivity.java
+++ app/src/main/java/com/example/androidgilla42b/EvenementEditActivity.java
@@ -1,14 +1,334 @@
 package com.example.androidgilla42b;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class EvenementEditActivity extends AppCompatActivity {
+
+    private static final String TAG_ID = "id";
+    private static final String TAG_DATE_TIME = "date_time";
+    private static final String TAG_TITLE = "title";
+    private static final String TAG_DESCRIPTION = "description";
+
+    String method = "";
+    String aid;
+    String date_time = "";
+    String title = "";
+    String description = "";
+
+    // contrôles de la vue (layout) correspondante
+    EditText edtDate_time, edtTitle, edtDescription;
+    TextView txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+
+    AlertDialog.Builder alert;
+
+    private void setDateTimeStr(){
+        SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        if (Objects.equals(date_time, "")){
+            Calendar rightNow = Calendar.getInstance();
+            date_time = dateTimeFormat.format(rightNow.getTime());
+        } else {
+            Calendar mCalendar = Calendar.getInstance();
+            mCalendar.set(Calendar.YEAR, mYear);
+            mCalendar.set(Calendar.MONTH, mMonth);
+            mCalendar.set(Calendar.DAY_OF_MONTH, mDay);
+            mCalendar.set(Calendar.HOUR_OF_DAY, mHour);
+            mCalendar.set(Calendar.MINUTE, mMinute);
+            mCalendar.set(Calendar.SECOND, 0);
+            mCalendar.set(Calendar.MILLISECOND, 0);
+            date_time = dateTimeFormat.format(mCalendar.getTime());
+            edtDate_time.setText(date_time, TextView.BufferType.EDITABLE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evenement_edit);
+
+        // affichage du titre de l'activité dans la barre applicative (app bar)
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getString(R.string.title_gilla) + " : " +  getString(R.string.evenement));
+
+        // récupération de l'intent de la vue de détail :
+        Intent in = getIntent();
+
+        // obtention de l'aid de l'évènement à partir de l'intent :
+        try {
+            aid = in.getStringExtra(TAG_ID);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (Objects.isNull(aid)){
+            // cas de la création d'un évènement
+            method = "POST";
+            setDateTimeStr();
+        } else {
+            // cas de la modification d'un évènement
+            method = "PATCH";
+            date_time = in.getStringExtra(TAG_DATE_TIME);
+            title = in.getStringExtra(TAG_TITLE);
+            description = in.getStringExtra(TAG_DESCRIPTION);
+        }
+
+        // association des éléments du layout aux objets correspondants :
+
+
+        edtDate_time = (EditText) findViewById(R.id.edtDate_time);
+        txtDate=(TextView) findViewById(R.id.in_date);
+        txtTime=(TextView) findViewById(R.id.in_time);
+
+        edtTitle = (EditText) findViewById(R.id.edtTitle);
+        edtDescription = (EditText) findViewById(R.id.edtDescription);
+
+
+
+
+        // initialisation des objets de type EditText :
+        edtDate_time.setText(date_time, TextView.BufferType.EDITABLE);
+        edtTitle.setText(title, TextView.BufferType.EDITABLE);
+        edtDescription.setText(description, TextView.BufferType.EDITABLE);
+
+        SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date dateTimeObj = null;
+        try {
+            dateTimeObj = dateTimeFormat.parse(date_time);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        String dateStr = DateFormat.getDateInstance(DateFormat.FULL).format(dateTimeObj);
+        txtDate.setText(dateStr);
+        String timeStr = DateFormat.getTimeInstance(DateFormat.SHORT).format(dateTimeObj);
+        txtTime.setText(timeStr);
+
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+        SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+        SimpleDateFormat hourFormat = new SimpleDateFormat("hh");
+        SimpleDateFormat minuteFormat = new SimpleDateFormat("mm");
+        mYear = Integer.parseInt(yearFormat.format(dateTimeObj));
+        mMonth = Integer.parseInt(monthFormat.format(dateTimeObj)) - 1;
+        mDay = Integer.parseInt(dayFormat.format(dateTimeObj));
+        mHour = Integer.parseInt(hourFormat.format(dateTimeObj));
+        mMinute = Integer.parseInt(minuteFormat.format(dateTimeObj));
+
+        txtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ouvre la boîte de dialogue DatePicker
+                DatePickerDialog dpDialog = new DatePickerDialog(EvenementEditActivity.this,
+                        (view, year, month, day) -> {
+                            Calendar mCalendar = Calendar.getInstance();
+                            mCalendar.set(Calendar.YEAR, year);
+                            mCalendar.set(Calendar.MONTH, month);
+                            mCalendar.set(Calendar.DAY_OF_MONTH, day);
+                            String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
+                            txtDate.setText(selectedDate);
+
+                            mYear = year;
+                            mMonth = month;
+                            mDay = day;
+                            setDateTimeStr();
+                        }, mYear, mMonth, mDay);
+                dpDialog.show();
+            }
+        });
+
+        txtTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ouvre la boîte de dialogue TimePicker
+                TimePickerDialog tpDialog = new TimePickerDialog(EvenementEditActivity.this,
+                        (view, hour, minute) -> {
+                            txtTime.setText(hour + ":" + minute);
+
+                            // HOUR24_MINUTE !?
+                            // Calendar mCalendar = Calendar.getInstance();
+                            // mCalendar.set(Calendar.YEAR, mYear);
+                            // mCalendar.set(Calendar.MONTH, mMonth);
+                            // mCalendar.set(Calendar.DAY_OF_MONTH, mDay);
+                            // String selectedDate = DateFormat.getDateInstance(DateFormat.HOUR24_MINUTE).format(mCalendar.getTime());
+                            // txtDate.setText(selectedDate);
+
+                            mHour = hour;
+                            mMinute = minute;
+                            setDateTimeStr();
+                        }, mHour, mMinute, true);
+                tpDialog.show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * Tâche de fond pour poster l'ajout de l'item par une requête HTTP :
+     */
+    class SaveItem extends AsyncTask<String, String, JSONObject> {
+
+        // url pour enregistrer un élément
+        String apiUrl = "";
+        // la clé applicative (apikey) est le jeton du super-utilisateur Joomla 4 (SLAM) :
+        String apiKey = getString(R.string.x_joomla_token);
+
+        JSONParser jsonParser = new JSONParser();
+
+        private ProgressDialog pDialog;
+
+        private static final String TAG_DATA = "data";
+        private static final String TAG_ATTRIBUTES = "attributes";
+
+        // affiche une barre de progression avant d'activer la tâche de fond
+        @Override
+        protected void onPreExecute() {
+            // super.onPreExecute();
+            pDialog = new ProgressDialog(EvenementEditActivity.this);
+            pDialog.setMessage("Attente de connexion...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+
+            SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+            apiUrl = "http://" + SP.getString("PREF_API_URL_LOC", getString(R.string.pref_default_api_url_loc)) + "/gilla/events";
+            String prefAPI = SP.getString("PREF_API", "0");
+            if (prefAPI.equals("1")) {
+                apiUrl = "https://" + SP.getString("PREF_API_URL_DIST", getString(R.string.pref_default_api_url_dist)) + "/gilla/events";
+            }
+            // Toast.makeText(EvenementEditActivity.this,"URL de l'API : " + apiUrl,Toast.LENGTH_LONG).show();
+        }
+
+        // obtention en tâche de fond de la réponse au format JSON par une requête HTTP
+        @Override
+        protected JSONObject doInBackground(String... args) {
+            try {
+                HashMap<String, String> hmUrlParams = new HashMap<>();
+                HashMap<String, String> hmBodyParams = new HashMap<>();
+                hmBodyParams.put("date_time", edtDate_time.getText().toString());
+                hmBodyParams.put("title", edtTitle.getText().toString());
+                hmBodyParams.put("description", edtDescription.getText().toString());
+                Log.d("request", "starting");
+                JSONObject json = jsonParser.makeHttpRequest(apiUrl, apiKey, method, aid, hmUrlParams, hmBodyParams);
+                if (json != null) {
+                    Log.d("JSON result", json.toString());
+                    return json;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(EvenementEditActivity.this,"Requête invalide !",Toast.LENGTH_LONG).show();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(JSONObject json) {
+            int success = 0;
+
+            if (pDialog != null && pDialog.isShowing()) {
+                pDialog.dismiss();
+            }
+
+            if (json != null) {
+                // Toast.makeText(EvenementEditActivity.this, json.toString(), Toast.LENGTH_LONG).show();
+                Log.d("Success!", "Connexion réussie");
+                // Details de la réponse reçue :
+                try {
+                    JSONObject objData = json.getJSONObject(TAG_DATA);
+                    JSONObject objAttrib = objData.getJSONObject(TAG_ATTRIBUTES);
+
+                    success = 1;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.d("Failure", "Echec de connexion)");
+            }
+
+            if (success == 1) {
+                // Enregistrement réussi :
+                // retourne à la liste des évènements
+                startActivity(new Intent(getApplicationContext(), EvenementsActivity.class));
+            } else {
+                // Echec de l'enregistrement !
+                alert = new AlertDialog.Builder(EvenementEditActivity.this);
+                alert.setMessage("Saisie incorrecte : réessayer !");
+                alert.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                alert.show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //ajoute les entrées de menu_test à l'ActionBar
+        getMenuInflater().inflate(R.menu.menu_evenement_edit, menu);
+        return true;
+    }
+
+    //gère le click sur une action de l'ActionBar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // gère la sélection d'un élément du menu :
+        switch (item.getItemId()){
+            case R.id.action_save:
+                if (edtTitle.getText().toString().equals("") || edtDescription.getText().toString().equals(""))
+                {
+                    Toast.makeText(EvenementEditActivity.this, getString(R.string.warning_save), Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                else
+                {
+                    // enregistrement de l'élément en fil d'exécution de fond (background thread)
+                    new SaveItem().execute();
+                    return true;
+                }
+//                return true;
+            case R.id.action_back:
+                startActivity(new Intent(this, EvenementsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/androidgilla42b/JSONParser.java
+++ app/src/main/java/com/example/androidgilla42b/JSONParser.java
@@ -1,2 +1,200 @@
-package com.example.androidgilla42b;public class JSONParser {
+package com.example.androidgilla42b;
+
+        import android.util.Log;
+
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
+        import java.io.BufferedInputStream;
+        import java.io.BufferedReader;
+        import java.io.DataOutputStream;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.io.UnsupportedEncodingException;
+        import java.net.HttpURLConnection;
+        import java.net.URL;
+        import java.net.URLEncoder;
+        import java.nio.charset.StandardCharsets;
+        import java.util.HashMap;
+        import java.util.Objects;
+
+/**
+ * Classe JSONParser contenant la méthode makeHttpRequest()
+ * permettant de construire et envoyer la requête http
+ * avec l'url, la méthode POST ou GET et les paramètres fournis
+ * et retourne un objet JSON contenant la réponse du serveur.
+ */
+
+public class JSONParser {
+    String charset = "UTF-8";
+    HttpURLConnection conn;
+    DataOutputStream wr;
+    StringBuilder result;
+    URL urlObj;
+    JSONObject jObj = null;
+    StringBuilder urlParams;
+    StringBuilder bodyParams;
+    String paramsString;
+
+    protected static String convertBody(String str) {
+        // A faire : les caractères accentués doivent être convertis en ISO_8859_1.
+        // Exemple : é => Ã© (comme fait dans Postman).
+        try {
+            byte[] buffer = StandardCharsets.ISO_8859_1.encode(str).array();
+            String strDecode = new String(buffer);
+            return strDecode;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public JSONObject makeHttpRequest(String url, String apiKey, String method, String urlParam,
+                                      HashMap<String, String> hmUrlParams, HashMap<String, String> hmBodyParams) {
+
+        urlParams = new StringBuilder();
+        int i = 0;
+        for (String key : hmUrlParams.keySet()) {
+            try {
+                if (i != 0){
+                    urlParams.append("&");
+                }
+                urlParams.append(key).append("=")
+                        .append(URLEncoder.encode(hmUrlParams.get(key), charset));
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
+
+        bodyParams = new StringBuilder();
+        int j = 0;
+        for (String key : hmBodyParams.keySet()) {
+            if (j == 0){
+                bodyParams.append("{\"");
+            } else {
+                bodyParams.append(",\"");
+            }
+            bodyParams.append(key).append("\": \"").append(hmBodyParams.get(key)).append("\"");
+            // bodyParams.append(key).append("\": \"").append(convertBody(hmBodyParams.get(key))).append("\"");
+            j++;
+        }
+        if (bodyParams.length() != 0) {
+            bodyParams.append("}");
+            paramsString = bodyParams.toString();
+        }
+
+        if (method.equals("POST")) {
+            // préparation et envoi de la requête http en POST :
+            try {
+                urlObj = new URL(url);
+                conn = (HttpURLConnection) urlObj.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestMethod(method);
+                conn.addRequestProperty("Content-Type", "application/json");
+                conn.addRequestProperty("X-Joomla-Token", apiKey);
+                conn.addRequestProperty("Accept", "*/*");
+                // conn.setRequestProperty("Accept-Charset", charset);
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.connect();
+                wr = new DataOutputStream(conn.getOutputStream());
+                wr.writeBytes(paramsString);
+                wr.flush();
+                wr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if(method.equals("GET")){
+            // préparation et envoi de la requête http en GET :
+            if (!Objects.isNull(urlParam)) {
+                url += "/" + urlParam;
+            } else if (urlParams.length() != 0) {
+                url += "?" + urlParams.toString();
+            }
+            try {
+                urlObj = new URL(url);
+                conn = (HttpURLConnection) urlObj.openConnection();
+                conn.setDoOutput(false);
+                conn.setRequestMethod(method);
+                conn.addRequestProperty("X-Joomla-Token", apiKey);
+                conn.addRequestProperty("Accept", "*/*");
+                // conn.setRequestProperty("Accept-Charset", charset);
+                conn.setConnectTimeout(15000);
+                conn.connect();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if(method.equals("PATCH")){
+            // préparation et envoi de la requête http en PATCH :
+            if (!Objects.isNull(urlParam)) {
+                url += "/" + urlParam;
+            }
+            try {
+                urlObj = new URL(url);
+                conn = (HttpURLConnection) urlObj.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestMethod(method);
+                conn.addRequestProperty("Content-Type", "application/json");
+                conn.addRequestProperty("X-Joomla-Token", apiKey);
+                conn.addRequestProperty("Accept", "*/*");
+                // conn.setRequestProperty("Accept-Charset", charset);
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.connect();
+                wr = new DataOutputStream(conn.getOutputStream());
+                wr.writeBytes(paramsString);
+                wr.flush();
+                wr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if(method.equals("DELETE")){
+            // préparation et envoi de la requête http en PATCH :
+            if (!Objects.isNull(urlParam)) {
+                url += "/" + urlParam;
+            }
+            try {
+                urlObj = new URL(url);
+                conn = (HttpURLConnection) urlObj.openConnection();
+                conn.setDoOutput(false);
+                conn.setRequestMethod(method);
+                conn.addRequestProperty("X-Joomla-Token", apiKey);
+                conn.addRequestProperty("Accept", "*/*");
+                // conn.setRequestProperty("Accept-Charset", charset);
+                conn.setConnectTimeout(15000);
+                conn.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            // réception de la réponse du serveur :
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            result = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            Log.d("JSON Parser", "result: " + result.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        conn.disconnect();
+
+        // parse la chaîne de caractères reçue en un objet JSON :
+        try {
+            jObj = new JSONObject(result.toString());
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // retourne l'objet JSON :
+        return jObj;
+    }
 }
